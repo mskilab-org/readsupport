50e2ee6c2310646cd9569ae9ba3ced80e68daa9d
Zi-Ning Choo <zining01@gmail.com>
2022-02-14 13:49:59 -0500

    fix unit tests

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -12,13 +12,32 @@ bwa = BWA(seq = ref)
 
 test_that('junction.support', {
 
-  res = junction.support(reads, junction, realign = FALSE)
-  expect_equal(length(res), 49)
-  expect_equal(length(unique(res$qname)), 18)
+  res.norealn = junction.support(reads, junction, realign = FALSE, pad.ref = 1e3)
+  expect_equal(length(res.norealn), 51)
+  expect_equal(length(unique(res.norealn$qname)), 18)
 
-  ##  res = junction.support(reads, junction, ref = ref, bwa = bwa, realign = TRUE, pad.ref = 1e3)
+  res = junction.support(reads, junction, ref = ref, bwa = bwa, realign = TRUE, pad.ref = 1e3)
+  expect_true(length(res) > 30)
+  expect_true(length(unique(res$qname)) > 15)
+})
+
+## sdqn = setdiff(res.norealn$qname, res$qname)
+## sdres = res.norealn %Q% (qname %in% sdqn)
+
+## grl = split(res, res$qname)
+## sdgrl = split(sdres, sdres$qname)
+
+## rg = stack(junction$grl)
+## gw = gW(grl = grl)
+## sdgw = gW(grl = sdgrl)
+## gt = gTrack(grl, labels.suppress.grl = TRUE)
+## sdgt = gTrack(sdgrl, labels.suppress.grl = TRUE)
+
+## ppng(plot(c(sdgt, gt), rg + 1e3, links = junction$grl))
+
+
   ##  expect_equal(length(res), 49)
   ##  expect_equal(length(unique(res$qname)), 18)
   
-})
+## })
 
